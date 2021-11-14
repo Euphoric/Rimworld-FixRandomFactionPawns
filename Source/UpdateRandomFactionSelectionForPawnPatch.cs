d9a2133404ea3b401cadf7c7c5243671f2f5a889
Radek Falhar <Radek.Falhar@hotmail.com>
2021-11-14 11:21:56 +0100

    Patch Pawn.Notify_PassedToWorld to select only from allowed pawn factions

---
+++ Source/UpdateRandomFactionSelectionForPawnPatch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Euphoric.FixRandomFactionPawns
+{
+    /// <summary>
+    /// When adding pawn to a world, Rimworld can select different random faction for pawn if current faction is not valid.
+    /// This replaces the random selection method with one that only selects from factions allowed for the pawn def kind.
+    /// </summary>
+    [HarmonyPatch]
+    internal static class UpdateRandomFactionSelectionForPawnPatch
+    {
+        [HarmonyPatch(typeof(Pawn), "Notify_PassedToWorld")]
+        [HarmonyTranspiler]
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var originalMethod = AccessTools.Method(typeof(FactionManager), "TryGetRandomNonColonyHumanlikeFaction");
+            var newMethod = AccessTools.Method(typeof(FactionHelper), nameof(FactionHelper.TryGetRandomFactionForPawn));
+            foreach (var instruction in instructions)
+            {
+                if (instruction.Calls(originalMethod))
+                {
+                    yield return new CodeInstruction(OpCodes.Ldarg_0); // loads Pawn instance
+                    yield return new CodeInstruction(OpCodes.Callvirt, newMethod);
+                }
+                else
+                {
+                    yield return instruction;
+                }
+            }
+        }
+    }
+}
